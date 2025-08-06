15153e1986fc50ada8f29e730eaaa266dc18bd89
Audrey <jadoratan@gmail.com>
2025-08-06 00:18:41 -0700

    All elements functional
    TODO: add copy results button?

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+// Global variables
 var debug = false;
 var questions_array = []; //to be filled
-const msg = document.getElementById("messages");
-const list = document.getElementById("list");
+
+// Elements
+const div_msg = document.getElementById("messages-div");
+const list_questions = document.getElementById("list");
+const button_submit = document.getElementById("submit");
+const button_reset = document.getElementById("reset");
+const text_score = document.getElementById("score");
 
 if (debug) {
   const init_msg = document.createElement("p");
   init_msg.textContent = "Initialized msg and list";
-  msg.appendChild(init_msg);
+  div_msg.appendChild(init_msg);
 }
 
+// Event Listeners
+button_submit.addEventListener("click", calculateScore);
+button_reset.addEventListener("click", reset);
 
+// Fetch and populate list of questions
 fetch("list.csv")
   .then(response => response.text())
   .then(csv => {
@@ -18,28 +28,29 @@ fetch("list.csv")
     if (debug) {
       const fetch_msg = document.createElement("p");
       fetch_msg.textContent = "Array length: " + questions_array.length;
-      msg.appendChild(fetch_msg); 
+      div_msg.appendChild(fetch_msg); 
     }
 
-    load_questions();
+    loadQuestions();
     
   })
   .catch((error) => {
     const error_msg = document.createElement("p");
     error_msg.textContent = error;
-    msg.appendChild(error_msg);  
+    div_msg.appendChild(error_msg);  
   })
-  .finally()
 
 
-function load_questions() {
+// Functions
+
+function loadQuestions() {
   for (var i=0; i < questions_array.length; i++) {
         const li = document.createElement("li");
 
         if (debug && i==0) {
           const li_msg = document.createElement("p");
           li_msg.textContent = "Created list";
-          msg.appendChild(li_msg);
+          div_msg.appendChild(li_msg);
         }
 
         const checkbox = document.createElement("input");
@@ -55,6 +66,32 @@ function load_questions() {
         li.appendChild(checkbox);
         li.appendChild(label);
 
-        list.appendChild(li);
+        list_questions.appendChild(li);
     }
+}
+
+
+function calculateScore() {
+  const checked = document.querySelectorAll('input[type="checkbox"]:checked').length;
+  const score = questions_array.length - checked;
+  
+  text_score.textContent = `Score: ${score}`;
+
+  return score;
+}
+
+function reset() {
+  const checked = document.querySelectorAll('input[type="checkbox"]:checked');
+
+  for (const box of checked) {
+    box.checked = false;
+  }
+  // OR
+  // checked.forEach(cb => cb.checked = false);
+
+  if (debug) {
+    const reset_msg = document.createElement("p");
+    reset_msg.textContent = "reset";
+    div_msg.appendChild(reset_msg);
+  }
 }
